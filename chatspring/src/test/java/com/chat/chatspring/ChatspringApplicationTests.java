f24370d10aa387560d3765a22c6a90249f44b168
Misterxof <mishail.lukushevich@gmail.com>
2017-12-20 23:10:19 +0300

    init

---
+++ chatspring/src/test/java/com/chat/chatspring/ChatspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chat.chatspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
